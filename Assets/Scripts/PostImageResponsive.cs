1f2b28aaaac0fbf5aaa4daebbd2ee40f28182c57
Shadman Arko <shadman.arko@gmail.com>
2021-11-03 12:56:33 +0600

    Initial Push

---
+++ Assets/Scripts/PostImageResponsive.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PostImageResponsive : MonoBehaviour
+{
+    public Sprite image;
+    public float aspectRatio;
+    
+    private void Start()
+    {
+        Debug.Log(image.rect.height);
+        Debug.Log(image.bounds.size.y);
+        aspectRatio = image.bounds.size.x / image.bounds.size.y;
+        gameObject.GetComponent<AspectRatioFitter>().aspectRatio = aspectRatio;
+        gameObject.GetComponent<Image>().sprite = image;
+    }
+}
